26ac212c24ae96876dda7e409d915d66a447ac5e
Aaron <leebaaron@outlook.com>
2026-04-20 20:40:21 -0500

    Attempting 1 & 2

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,5 @@
 # What is the hash of block 243,833?
+
+hash=$(bitcoin-cli -signet getblockhash 243833)
+
+echo $hash
--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,8 @@
 # How many new outputs were created by block 243,825?
+
+
+hash=$(bitcoin-cli -signet getblockhash 243825)
+
+block=$(bitcoin-cli -signet getblock $hash 2 | jq '[.tx[].vout | length] | add')
+
+
